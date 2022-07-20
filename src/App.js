c3d980b465e09340a9f9cfb3596da5120db4abd1
EduardoFlossOlbermann <eduardoolbermann@hotmail.com>
2022-07-19 21:07:17 -0300

    finalizado-filtros

--- src/App.js
+++ src/App.js
@@ -10,24 +10,51 @@ function App() {
   const [carrinho, setCarrinho] = useState([])
   const [produtoAtual] = useState(Produtos)
   const [consulta, setConsulta] = useState("")
-
+  const [minPreco, setMinPreco] = useState(-Infinity)
+  const [maxPreco, setMaxPreco] = useState(Infinity)
+  const [ordenar, setOrdenar] = useState("cresc")
+  const [ordernarSortido, setOrdenarSortido] = useState("nomeDoProduto")
   return (
     <DivApp>
 
       <Filtros
         consulta={consulta}
+        minPreco={minPreco}
+        maxPreco={maxPreco}
+        ordenar={ordenar}
+        ordernarSortido={ordernarSortido}
         setConsulta={setConsulta}
+        setMinPreco={setMinPreco}
+        setMaxPreco={setMaxPreco}
+        setOrdenar={setOrdenar}
+        setOrdenarSortido={setOrdenarSortido}
       />
 
       <DivProdutosExibidos>
         {
           produtoAtual
-          .filter(prod => prod.nomeDoProduto.includes(consulta))
-          .map(prod => <ListaDeProdutos key={prod.id} prod={prod} setCarrinho={setCarrinho} carrinho={carrinho}></ListaDeProdutos>)
+            .filter(prod => prod.nomeDoProduto.includes(consulta))
+            .filter(prod => prod.valor >= minPreco || minPreco === "")
+            .filter(prod => prod.valor <= maxPreco || maxPreco === "")
+            .sort((atualProduto, proximoProduto) => {
+              switch (ordernarSortido) {
+                case "valor":
+                  return atualProduto.valor - proximoProduto.valor
+                case "nomeDoProduto":
+                  return atualProduto.nomeDoProduto.localeCompare(proximoProduto.nomeDoProduto)
+                default:
+              }
+            })
+            .sort(() => {
+              if (ordenar === "cresc") {
+                return 0
+              } else {
+                return -1
+              }
+            })
+            .map(prod => <ListaDeProdutos key={prod.id} prod={prod} setCarrinho={setCarrinho} carrinho={carrinho}></ListaDeProdutos>)
         }
       </DivProdutosExibidos>
-
-      <ListaDeProdutos meusProdutos={produtoAtual} adicionarAoCarrinho={setCarrinho} carrinho={carrinho} />
       <Carrinho carrinho={carrinho} />
     </DivApp>
   );
